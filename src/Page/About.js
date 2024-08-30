840271b6788606c8cd2f4bb5ee923b856c6c9df4
hp <niranjan962796@gmail.com>
2024-08-30 12:56:40 +0530

    third commit

--- src/Page/About.js
+++ src/Page/About.js
@@ -5,13 +5,17 @@ import "./About.css";
 
 const About=()=>{
     return(
+        <div>
         <div className="about-1">
            <div className="about-img">
 <img src="https://tse4.mm.bing.net/th?id=OIP.FWsRJleGAOHJVEEkWwYZpwHaE8&pid=Api&P=0&h=180"></img>
            </div>
            <div>
-            <h1>Download and use over 100,000 high-quality restaurant food image</h1>
+            <h1>ICC Cricket</h1>
+            <p style={{fontWeight:"700", fontSize:"20px"}}>Cricket is a bat-and-ball game that is played between two teams of eleven players</p>
            </div>
+          
+        </div>
         </div>
     )
 }
--- src/Page/Signup.js
+++ src/Page/Signup.js
@@ -1,12 +1,63 @@
+import { useState } from "react";
 
 
+function Signup(){
+
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage]= useState("");
+    const [error,setError]=useState(false);
 
 
-const Signup=()=>{
-    return(
-        <div>
-            Signup
+   
+
+    function onFormDataChange(e){
+
+        const id=e.target.id;
+
+        if(id==="userId"){
+            setUserId(e.target.value);
+        }
+        else if(id==="password"){
+            setPassword(e.target.value);
+        }
+
+    }
+
+    
+
+    function onSignIn(e){
+        e.preventDefault();
+        console.log(userId,password);
+    }
+
+    return <div className="bg-info d-flex justify-content-center align-items-center vh-100" >
+
+        <div style={{width:"30rem"}} className="card p-3 rounded-4 shadow-lg text-center">
+
+            <h4 className="text-info " > SignIn </h4>
+
+            <form onSubmit={onSignIn}>
+
+                <div className="input-group" >
+                    <input className="form-control m-1" onChange={onFormDataChange} value={userId} type="text" id="userId" placeholder="User Id"/>
+                </div>
+
+
+                <div  className="input-group" >
+                    <input className="form-control m-1"  onChange={onFormDataChange} value={password} type="password" id="password" placeholder="Password"/>
+                </div>
+
+
+                <div  className="input-group" >
+                    <input  className="btn btn-info text-white form-control m-1" type="submit" value="Sign In" />
+                    </div>
+            </form>
+
+            <div className={(error)?"text-danger":"text-success"} > {message} </div>
+
         </div>
-    )
+       </div>
+
 }
 export default Signup;
